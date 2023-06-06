e2c38fe0733b7035f15cc49c309d1e7497a45207
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2023-06-06 18:26:00 -0300

    add summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../../Components/Header"
+import Summary from "../../Components/Summary"
 
 function Transactions() {
   return(
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
