4e5f91bf7dfc858ab84332c235bede9299c2a076
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2023-06-06 21:58:45 -0300

    add modal

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,12 +1,28 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
 import Logo from "../../assets/Logo.svg"
+import * as Dialog from "@radix-ui/react-dialog"
 
 function Header() {
   return(
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
